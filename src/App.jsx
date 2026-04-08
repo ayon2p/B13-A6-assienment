e567f5c3457aaeb1340a5b92c5cf4bbf273b0f66
ayon2p <ayonpaul171921@gmail.com>
2026-04-08 11:54:19 +0600

    app jsx style

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,55 @@
 
-import './App.css'
+import { Suspense, useState } from 'react'; 
+import './App.css';
+import HeroSection from './componant/HeroSection/HeroSection'; 
+import Navbar from './componant/Navbar/Navbar';
+import ProductCard from './componant/ProductCard/ProductCard';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { toast } from 'react-toastify';
+import GetStarted from './componant/StartedStep/GetStarted';
+import Pricing from './componant/pricing/Pricing';
+import Footer from './componant/footer/Footer';
+const Product = async () => {
+    const res = await fetch("/data.json");
+    return res.json();
+}
 
 function App() {
+  const [cart, setCart] = useState([]); 
+
+
+const handleAddToCart = (item) => {
+  setCart((prevCart) => [...prevCart, item]);
+ 
+  toast.success(`${item.title} added to cart!`, {
+    position: "top-right",
+    autoClose: 2000,
+  });
   
+};
+  const ProductCardPromise = Product();
 
   return (
     <>
-     
-          <h1>Get started</h1>
-         
+  
+      <Navbar cartCount={cart.length} />
+      <HeroSection />
+      <Suspense fallback={<span>Loading...</span>}>
+        
+        <ProductCard 
+          ProductCardPromise={ProductCardPromise} 
+          cart={cart}
+          setCart={setCart} 
+          onAddToCart={handleAddToCart} 
+        />
+      </Suspense>
+      <GetStarted></GetStarted>
+    <Pricing></Pricing>
+    <Footer></Footer>
+     <ToastContainer></ToastContainer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
