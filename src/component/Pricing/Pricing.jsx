b457339337b0fb0e58231cc619d01a736ca5b4a4
ayon2p <ayonpaul171921@gmail.com>
2026-04-08 12:07:22 +0600

    pricing style design :

---
+++ src/component/Pricing/Pricing.jsx
@@ -0,0 +1,118 @@
+import React from 'react';
+
+const Pricing = () => {
+    return (
+    <section className='max-w-7xl mx-auto'>
+<div className='grid grid-cols-3'>
+                 <div className="card w-96 bg-base-100 shadow-sm">
+                  <div className="card-body">
+                 <div className="space-y-4">
+                   <h2 className="text-3xl font-bold">Starter</h2>
+                   <p>Perfect for getting started</p>
+                   <p className="text-xl"><span className='text-4xl'>$0</span>/mo</p>
+                 </div>
+                 <ul className="mt-6 flex flex-col gap-2 text-xs">
+                   <li>
+                     <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                     <span className='text-xl'>Access to 10 free tools</span>
+                   </li>
+                   <li>
+                     <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                     <span className='text-xl'>Basic templates</span>
+                   </li>
+                   <li>
+                     <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                     <span className='text-xl'>Community support</span>
+                   </li>
+                   <li>
+                     <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                     <span className='text-xl'>1 project per month</span>
+                   </li>
+                 
+                 </ul>
+                 <div className="mt-6">
+                   <button className="btn btn-primary w-full rounded-full px-6 normal-case text-white bg-gradient-to-r from-purple-600 to-blue-500 border-none">Get Started Free</button>
+                 </div>
+               </div>
+               </div>
+
+                 <div className="card w-96 bg-base-100 shadow-sm normal-case text-white bg-gradient-to-r from-purple-600 to-blue-500 border-none">
+     <div className="card-body">
+    <div className="space-y-4">
+      <h2 className="text-3xl font-bold">Pro</h2>
+      <p>Best for professionals</p>
+      <p className="text-xl"><span className='text-4xl'>$29</span>/mo</p>
+    </div>
+    <ul className="mt-6 flex flex-col gap-2 text-xs">
+      <li>
+        <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+        <span className='text-xl'>Access to all premium tools</span>
+      </li>
+      <li>
+        <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+        <span className='text-xl'>Unlimited templates</span>
+      </li>
+      <li>
+        <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+        <span className='text-xl'>Priority support</span>
+      </li>
+      <li>
+        <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+        <span className='text-xl'>Unlimited projects</span>
+      </li>
+      <li>
+        <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+        <span className='text-xl'>Cloud sync</span>
+      </li>
+      <li>
+        <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+        <span className='text-xl'>Advanced analytics</span>
+      </li>
+    
+    </ul>
+    <div className="mt-6">
+      <button className="btn bg-white text-black border-[#e5e5e5] rounded-full w-full"><span className= " font-extrabold bg-gradient-to-r from-purple-400 to-blue-400 bg-clip-text text-transparent">Start Pro Trial</span></button>
+    </div>
+  </div>
+</div>
+
+
+
+
+                 <div className="card w-96 bg-base-100 shadow-sm ">
+     <div className="card-body">
+    <div className="space-y-4">
+        <h1 className='text-3xl font-bold'>Enterprise</h1>
+      <p>For teams and businesses</p>
+      <p className="text-xl"><span className='text-4xl'>$99</span>/mo</p>
+    </div>
+    <ul className="mt-6 flex flex-col gap-2 text-xs">
+      <li>
+        <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+        <span className='text-xl'>Everything in Pro</span>
+      </li>
+      <li>
+        <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+        <span className='text-xl'>Team collaboration</span>
+      </li>
+      <li>
+        <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+        <span className='text-xl'>Custom integrations</span>
+      </li>
+      <li>
+        <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+        <span className='text-xl'>Dedicated support</span>
+      </li>
+    
+    </ul>
+    <div className="mt-6">
+      <button className="btn btn-primary w-full rounded-full px-6 normal-case text-white bg-gradient-to-r from-purple-600 to-blue-500 border-none">Contact Sale</button>
+    </div>
+  </div>
+</div>
+              </div>
+    </section>
+    );
+};
+
+export default Pricing;
